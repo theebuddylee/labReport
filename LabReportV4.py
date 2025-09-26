480c0ac63c9e62019d2733f425b1c87f35db006c
theebuddylee <buddyt133@gmail.com>
2025-09-26 11:10:40 -0700

    Update LabReportV4.py
    Shifted font size on insert_text for cover page

--- LabReportV4.py
+++ LabReportV4.py
@@ -704,9 +704,9 @@ def overwrite_more_information(template_path, output_path, member_name, selected
         manager_phone = manager_info.get("phone", "N/A")
         doc = fitz.open(template_path)
         first_page = doc[0]
-        first_page.insert_text((70, 538), f"{member_name or 'Not Provided'}", fontsize=18, color=(0.451, 0.451, 0.451))
-        first_page.insert_text((70, 577), f"{manager_phone}", fontsize=18, color=(0.451, 0.451, 0.451))
-        first_page.insert_text((70, 615), f"{manager_email}", fontsize=18, color=(0.451, 0.451, 0.451))
+        first_page.insert_text((70, 538), f"{member_name or 'Not Provided'}", fontsize=17, color=(0.451, 0.451, 0.451))
+        first_page.insert_text((70, 577), f"{manager_phone}", fontsize=17, color=(0.451, 0.451, 0.451))
+        first_page.insert_text((70, 615), f"{manager_email}", fontsize=17, color=(0.451, 0.451, 0.451))
         doc.save(output_path)
         return output_path
     except Exception as e:
@@ -961,6 +961,7 @@ if st.button("Generate PDF"):
 
 st.text(f"Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 conn.close()
+
 
 
 
